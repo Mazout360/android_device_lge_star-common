9ccc175a5d342850fd5e643822917d294cd40535
tonypp <text2tony@gmail.com>
2012-08-23 04:46:38 +0200

    override to use MTP

--- star.mk
+++ star.mk
@@ -81,7 +81,7 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
 # Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
