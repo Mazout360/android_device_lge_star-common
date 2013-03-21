2e08b7641a655ed490520273c5d6e5d20015376d
tonyp <tonyptonyp@gmail.com>
2013-03-21 02:38:16 +0100

    it's getting desperate

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2012 The CyanogenMod Project <http://www.cyanogenmod.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +18,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME "LG Optimus 2x"
+#define BTM_DEF_LOCAL_NAME   "GT-I9103"
 
 #endif
